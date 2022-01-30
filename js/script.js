1eadf86b15c5a4b13540a67997c28fdd5ddea289
AlpSnow <max-wallis@outlook.fr>
2022-01-31 00:42:13 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let carre1 = document.querySelector(".carre1");
+
+carre1.addEventListener("click", function() {
+    carre1.classList.toggle("activeLeCSS1");
+})
+
+
+let carre2 = document.querySelector(".carre2");
+
+carre2.addEventListener("click", function() {
+    carre2.classList.toggle("activeLeCSS2");
+})
+
+
+let carre3 = document.querySelector(".carre3");
+
+carre3.addEventListener("click", function() {
+    carre3.classList.toggle("activeLeCSS3");
+})
